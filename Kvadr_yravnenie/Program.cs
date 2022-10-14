dc80f067b1fa7926b3cd0ea3c53d31b94b7be60c
Надежда <@DESKTOP-F7NLP93>
2022-10-15 00:29:58 +0700

    Добавьте файлы проекта.

---
+++ Kvadr_yravnenie/Program.cs
@@ -0,0 +1,23 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите a, b, c");
+    double a = Convert.ToDouble(Console.ReadLine());
+    double b = Convert.ToDouble(Console.ReadLine());
+    double c = Convert.ToDouble(Console.ReadLine());
+    double d = b * b - 4 * a * c;
+    if (d < 0)
+    {
+        Console.WriteLine("Корней нет");
+    }
+    if (d == 0)
+    {
+        double x = (-b / 2 * a);
+        Console.WriteLine($"Корень равен {x}");
+    }
+    if (d > 0)
+    {
+        double x1 = (-b - (d * d) / 2 * a);
+        double x2 = (-b + (d * d) / 2 * a);
+        Console.WriteLine($"Корни равны {x1}, {x2}");
+    }
+}
